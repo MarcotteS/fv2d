3c591b22c32d08bceca9416184b3810e7fc257fb
Maxime Delorme <maxime.delorme@cea.fr>
2023-08-01 09:16:19 +0200

    Logging contributions to energy + boost to heating

--- main.cpp
+++ main.cpp
@@ -34,6 +34,7 @@ int main(int argc, char **argv) {
 
     // Misc vars for iteration
     real_t t = 0.0;
+    int save_ite = 0;
     int ite = 0;
     real_t next_save = 0.0;
     
@@ -69,7 +70,7 @@ int main(int argc, char **argv) {
 
       if (save_needed) {
         std::cout << " - Saving at time " << t << std::endl;
-        ioManager.saveSolution(Q, ite++, t);
+        ioManager.saveSolution(Q, save_ite++, t, dt);
         next_save += params.save_freq;
       }
 
@@ -78,6 +79,7 @@ int main(int argc, char **argv) {
       checkNegatives(Q, params);
 
       t += dt;
+      ite++;
     }
 
     std::cout << "Time at end is " << t << std::endl;
